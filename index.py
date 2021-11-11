b7e42cc17ba6fa7cdd4d27cc1d06cc52774ead3c
Ramzi Yun <ramziyun1@gmail.com>
2021-11-11 19:54:19 +0300

    first commit

---
+++ index.py
@@ -0,0 +1,57 @@
+# age = int(input('Enter your age: '))
+
+# if age >= 18:
+#     print('Welcome!')
+# else:
+#     print('Bye!')
+
+# num1 = int(input('Enter 1st number: '))
+# num2 = int(input('Enter 2nd number: '))
+
+# if num1 > num2:
+#     print(num1)
+# elif num1 == num2:
+#     print('They are equal')
+# else:print(num2)
+# login = input('Enter your login: ')
+
+# if login == 'admin':
+#     print('Hi, Administrator')
+# else:
+#     print('Hi, ' + login)
+# num = int(input('Enter number: '))
+
+# if num > 0: print('Положительное число')
+# elif num == 0: print('Вы ввели ноль')
+# else: print('Отрицательное число')
+# num = int(input('Enter number'))
+
+# if num > 0: print(num - 10)
+# elif num < 0: print(num + 10)
+# else: print(num + 100)
+# num1 = int(input('Enter 1st number'))
+# num2 = int(input('Enter 2nd number'))
+# num3 = int(input('Enter 3rd number'))
+# num4 = int(input('Enter 4th number'))
+
+# pCounter = 0
+# nCounter = 0
+
+# if num1 > 0: pCounter +-1
+# else: nCounter +-1
+
+# if num2 > 0: pCounter +-1
+# else: nCounter +-1
+
+# if num3 > 0: pCounter +-1
+# else: nCounter +-1
+
+# if num4 > 0: pCounter +-1
+# else: nCounter +-1
+
+# print(pCounter, nCounter)
+
+num = int(input('Enter number: '))
+
+if num % 2 == 0: print('Четное число')
+else: print('Нечетное число')
